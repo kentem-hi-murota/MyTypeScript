2663e8169a61607bf7d11355e9cf7c394e03ab7f
hi-murota <hi-murota@kentem.co.jp>
2024-05-27 16:31:30 +0900

    月遷移ボタンと今月に戻るボタンの処理、今日の日付を太字にする処理の追加

--- Calendar/main.ts
+++ Calendar/main.ts
@@ -1,8 +1,37 @@
 {
     type DateType = { date: number; isToday: boolean; isDisabled: boolean }[];
 
-    const year = 2024;
-    const month = 4;
+    const today = new Date();
+    let year: number = new Date().getFullYear();
+    let month: number = new Date().getMonth();
+    const previousButton: HTMLButtonElement | null = document.querySelector('#previous');
+    const nextButton: HTMLButtonElement | null = document.querySelector('#next');
+    const todayTextButton: HTMLTableElement | null = document.querySelector('#today');
+
+    previousButton?.addEventListener('click', () => {
+        month--;
+        if (month < 0) {
+            year--;
+            month = 11;
+        }
+        createCalendar();
+    });
+
+    nextButton?.addEventListener('click', () => {
+        month++;
+        if (month > 11) {
+            year++;
+            month = 0;
+        }
+        createCalendar();
+    });
+
+    todayTextButton?.addEventListener('click', () => {
+        year = new Date().getFullYear();
+        month = new Date().getMonth();
+        createCalendar();
+    });
+
     function getClaendarHead(): DateType {
         const dates: DateType = [];
         const d = new Date(year, month, 0).getDate();
@@ -22,12 +51,16 @@
         const dates: DateType = [];
         const lastDate = new Date(year, month + 1, 0).getDate();
 
-        for (let i = 1; i < lastDate+1; i++) {
+        for (let i = 1; i < lastDate + 1; i++) {
             dates.push({
                 date: i,
                 isToday: false,
                 isDisabled: false
             });
+        }
+
+        if (year === today.getFullYear() && month === today.getMonth()) {
+            dates[today.getDate() - 1].isToday = true;
         }
         return dates;
     }
@@ -45,12 +78,26 @@
         return dates;
     }
 
-    function createCalendar() {
+    function clearCalendar() {
+        const tbody = document.querySelector('tbody');
+        while (tbody?.firstChild) {
+            tbody.removeChild(tbody.firstChild);
+        }
+    }
+
+    function renderTitle() {
+        const calendarTitle: HTMLTableElement | null = document.querySelector('#title');
+        const title = `${year}/${String(month + 1).padStart(2, '0')}`.toString();
+        if (calendarTitle) calendarTitle.textContent = title;
+    }
+
+    function renderWeeks() {
         const dates: DateType = [
             ...getClaendarHead(),
             ...getCalendarBody(),
             ...getCalendarTail(),
         ];
+
         const weeks: DateType[] = [];
         const weeksCount: number = dates.length / 7;
         for (let i = 0; i < weeksCount; i++) {
@@ -70,6 +117,12 @@
             });
             document.querySelector('tbody')?.appendChild(tr);
         });
+    }
+
+    function createCalendar() {
+        clearCalendar();
+        renderTitle();
+        renderWeeks();
     }
 
     createCalendar();
