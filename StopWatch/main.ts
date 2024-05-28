70a2b1c3e45ed985109631d05e9e826f2f0db4b8
hi-murota <hi-murota@kentem.co.jp>
2024-05-28 10:58:32 +0900

    stopボタンの処理を実装

--- StopWatch/main.ts
+++ StopWatch/main.ts
@@ -4,18 +4,30 @@
     const stopButton: HTMLButtonElement | null = document.querySelector('#stop-button');
     const resetButton: HTMLButtonElement | null = document.querySelector('#reset-button');
 
-    startButton?.addEventListener('click', () => {
-        const startTime = Date.now();
-        setInterval(() => {
-            if (startButton) startButton.disabled = true;
-            const elapsedMiliSeconds = Date.now() - startTime;
+    let timerId: NodeJS.Timeout;
+    let elapsedTime: number = 0;
 
-            const elapsedMiliSecondsText = (elapsedMiliSeconds%1000).toString().padStart(3, '0');
+    startButton?.addEventListener('click', () => {
+        const startTime = Date.now() - elapsedTime;
+        if (startButton) startButton.disabled = true;
+        if (stopButton) stopButton.disabled = false;
+        
+        timerId = setInterval(() => {
+            const elapsedMiliSeconds = Date.now() - startTime;
+            const elapsedMiliSecondsText = (elapsedMiliSeconds % 1000).toString().padStart(3, '0');
             const elapsedSecondsText = Math.floor((elapsedMiliSeconds / 1000) % 60).toString().padStart(2, '0');
             const elapsedMinituesText = Math.floor(elapsedMiliSeconds / 1000 / 60).toString().padStart(2, '0');
 
             if (timerText) timerText.textContent = `${elapsedMinituesText}:${elapsedSecondsText}.${elapsedMiliSecondsText}`;
-
+            
+            elapsedTime = elapsedMiliSeconds;
         }, 1);
+    });
+
+    stopButton?.addEventListener('click', () => {
+        clearInterval(timerId);
+        if (startButton) startButton.disabled = false;
+        if (stopButton) stopButton.disabled = true;
+        if (resetButton) resetButton.disabled = false;
     });
 }
