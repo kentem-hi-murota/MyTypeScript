f985807359c7e0b53ab182dbe64657bd720f844b
hi-murota <hi-murota@kentem.co.jp>
2024-05-28 10:38:02 +0900

    スタートボタンの動作を実装

--- StopWatch/main.ts
+++ StopWatch/main.ts
@@ -1,1 +1,21 @@
+{
+    const timerText: HTMLParagraphElement | null = document.querySelector('#time-text');
+    const startButton: HTMLButtonElement | null = document.querySelector('#start-button');
+    const stopButton: HTMLButtonElement | null = document.querySelector('#stop-button');
+    const resetButton: HTMLButtonElement | null = document.querySelector('#reset-button');
 
+    startButton?.addEventListener('click', () => {
+        const startTime = Date.now();
+        setInterval(() => {
+            if (startButton) startButton.disabled = true;
+            const elapsedMiliSeconds = Date.now() - startTime;
+
+            const elapsedMiliSecondsText = (elapsedMiliSeconds%1000).toString().padStart(3, '0');
+            const elapsedSecondsText = Math.floor((elapsedMiliSeconds / 1000) % 60).toString().padStart(2, '0');
+            const elapsedMinituesText = Math.floor(elapsedMiliSeconds / 1000 / 60).toString().padStart(2, '0');
+
+            if (timerText) timerText.textContent = `${elapsedMinituesText}:${elapsedSecondsText}.${elapsedMiliSecondsText}`;
+
+        }, 1);
+    });
+}
