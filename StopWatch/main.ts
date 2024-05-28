c56e60071e341ee93fce71fb31938e27f53b9a1d
hi-murota <hi-murota@kentem.co.jp>
2024-05-28 11:23:09 +0900

    経過時間の処理方法を修正

--- StopWatch/main.ts
+++ StopWatch/main.ts
@@ -10,16 +10,16 @@
     startButton?.addEventListener('click', () => {
         const startTime = Date.now() - elapsedTime;
         setButtonState(true, false, true);
-        
+
         timerId = setInterval(() => {
-            const elapsedMiliSeconds = Date.now() - startTime;
-            const elapsedMiliSecondsText = (elapsedMiliSeconds % 1000).toString().padStart(3, '0');
-            const elapsedSecondsText = Math.floor((elapsedMiliSeconds / 1000) % 60).toString().padStart(2, '0');
-            const elapsedMinituesText = Math.floor(elapsedMiliSeconds / 1000 / 60).toString().padStart(2, '0');
+            const elapsedDate: Date = new Date(Date.now() - startTime);
+            const elapsedMiliSecondsText: string = elapsedDate.getMilliseconds().toString().padStart(3, '0');
+            const elapsedSecondsText: string = elapsedDate.getSeconds().toString().padStart(2, '0');
+            const elapsedMinituesText: string = elapsedDate.getMinutes().toString().padStart(2, '0');
 
             if (timerText) timerText.textContent = `${elapsedMinituesText}:${elapsedSecondsText}.${elapsedMiliSecondsText}`;
-            
-            elapsedTime = elapsedMiliSeconds;
+
+            elapsedTime = elapsedDate.getTime();
         }, 1);
     });
 
@@ -28,14 +28,14 @@
         setButtonState(false, true, false)
     });
 
-    resetButton?.addEventListener('click' , () => {
+    resetButton?.addEventListener('click', () => {
         clearInterval(timerId);
         setButtonState(false, true, true);
         elapsedTime = 0;
         if (timerText) timerText.textContent = '00:00.000';
     });
 
-    const setButtonState = (isDisableStartBtn:boolean, isDisableStopBtn:boolean, isDisableResetBtn:boolean):void => {
+    const setButtonState = (isDisableStartBtn: boolean, isDisableStopBtn: boolean, isDisableResetBtn: boolean): void => {
         if (startButton) startButton.disabled = isDisableStartBtn;
         if (stopButton) stopButton.disabled = isDisableStopBtn;
         if (resetButton) resetButton.disabled = isDisableResetBtn;
