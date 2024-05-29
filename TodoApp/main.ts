479cba3475bc56eb528acc856b8d575bb2926c93
hi-murota <hi-murota@kentem.co.jp>
2024-05-29 16:17:03 +0900

    選択したものを削除する処理の実装、ページ更新後もチェック状態が保存されるように変更

--- TodoApp/main.ts
+++ TodoApp/main.ts
@@ -1,23 +1,31 @@
 {
     type TodoObject = { id: number, title: string, isCompleted: boolean };
 
+    const saveTodos = () => { localStorage.setItem('todos', JSON.stringify(todos)); }
     const renderTodo = (todo: TodoObject) => {
         const input = document.createElement('input');
         input.type = 'checkbox';
         input.checked = todo.isCompleted;
+        input.addEventListener('change', () => {
+            todos.map(item => { if (item.id === todo.id) item.isCompleted = !item.isCompleted; });
+            saveTodos();
+        });
         const span = document.createElement('span');
         span.textContent = todo.title;
+
         const label = document.createElement('label');
         label.appendChild(input);
         label.appendChild(span);
+
         const button = document.createElement('button');
         button.textContent = 'x';
         button.addEventListener('click', () => {
             if (!confirm('OK?')) return;
             li.remove();
             todos = todos.filter(item => item.id !== todo.id);
-            localStorage.setItem('todos', JSON.stringify(todos));
+            saveTodos();
         });
+
         const li = document.createElement('li');
         li.appendChild(label);
         li.appendChild(button);
@@ -39,10 +47,19 @@
         }
         renderTodo(todo);
         todos.push(todo);
-        localStorage.setItem('todos', JSON.stringify(todos));
+        saveTodos();
         if (todoTitle) todoTitle.value = '';
         if (todoTitle) todoTitle.focus();
     });
+
+    document.querySelector('#purge-btn')?.addEventListener('click', () => {
+        if (!confirm('OK?')) return;
+        todos = todos.filter(todo => { return todo.isCompleted === false });
+        saveTodos();
+        document.querySelectorAll('#todos li')?.forEach(li => { li.remove(); });
+        renderTodos();
+    });
+
 
     const localStorageTodos = localStorage.getItem('todos');
     let todos: TodoObject[] = localStorageTodos === null ? [] : JSON.parse(localStorageTodos);
