5341f761833f68a168058fd542ed4b81591a1398
hi-murota <hi-murota@kentem.co.jp>
2024-05-29 13:45:30 +0900

    終了までの処理とマスの数を可変に変更

--- NumbersGame/main.ts
+++ NumbersGame/main.ts
@@ -1,7 +1,9 @@
 {
     class Board {
-        constructor() {
-            this.panels = [...Array(panelCount)].map((_, i) => new Panel());
+        constructor(game: Game) {
+            this.game = game;
+
+            this.panels = [...Array(this.game.getPanelCount()**2)].map((_, i) => new Panel(this.game));
 
             this.setup();
         };
@@ -14,19 +16,21 @@
         };
 
         activate = () => {
-            this.shuffleArray(numbers);
+            this.shuffleArray(this.game.getNumbers());
             this.panels.forEach((panel, i) => {
-                panel.activate(numbers[i]);
+                panel.activate(this.game.getNumbers()[i]);
             });
         }
 
         shuffleArray = (array: number[]): number[] => array.sort(() => 0.5 - Math.random());
 
+        game: Game;
         panels: Panel[];
     }
 
     class Panel {
-        constructor() {
+        constructor(game: Game) {
+            this.game = game;
             this.li = document.createElement('li');
             this.li.classList.add('pressed');
 
@@ -38,38 +42,75 @@
         getLi = () => this.li;
 
         activate = (num: number) => {
-            currentNum = 0;
             this.li.classList.remove('pressed');
             this.li.textContent = num.toString();
         }
 
         check = () => {
-            if (currentNum === Number(this.li.textContent)) {
+            if (this.game.getCurrentNum() === Number(this.li.textContent)) {
                 this.li.classList.add('pressed');
-                currentNum++;
+                this.game.addCurrentNum();
+                if (this.game.getCurrentNum() === this.game.getPanelCount() ** 2) {
+                    clearInterval(this.game.getTimerId());
+                }
             }
         }
 
+        game: Game;
         li: HTMLLIElement;
     }
 
+    class Game {
+        constructor(level: number) {
+            this.panelCount = level;
+            this.numbers = [...Array(this.panelCount**2)].map((_, i) => i);
+            this.board = new Board(this);
+            this.timerId = undefined;
+            this.currentNum = undefined;
 
-    const panelCount = 4;
-    const numbers = [...Array(panelCount)].map((_, i) => i);
-    const startBtn: HTMLButtonElement | null = document.querySelector('#start-btn');
-    const timerText: HTMLParagraphElement | null = document.querySelector('#timer');
-    const board = new Board();
-    let timerId: NodeJS.Timeout;
+            const startBtn = document.querySelector('#start-btn');
+            startBtn?.addEventListener('click', () => {
+                this.start();
+            });
+            this.setup();
+        }
 
-    startBtn?.addEventListener('click', () => {
-        const startTime = Date.now();
-        board.activate();
+        getCurrentNum = () => this.currentNum;
+        addCurrentNum = () => { this.currentNum !== undefined ? this.currentNum++ : undefined; }
+        getTimerId = () => this.timerId;
+        getNumbers = () => this.numbers;
+        getPanelCount = () => this.panelCount;
 
-        timerId = setInterval(() => {
-            const elapsedtime = ((Date.now() - startTime) / 1000).toFixed(2);
-            if (timerText) timerText.textContent = elapsedtime.toString();
-        }, 10);
-    });
+        setup = () => {
+            const container: HTMLSelectElement | null = document.querySelector('#container');
+            const PANEL_WIDTH = 50;
+            const BOARD_PADDING = 10;
+            if (container) container.style.width = `${PANEL_WIDTH * this.getPanelCount() + BOARD_PADDING * 2}px`;
+        }
 
-    let currentNum = 0;
+        start = () => {
+            this.currentNum = 0;
+            this.board.activate();
+            this.runTimer();
+        }
+
+        runTimer = () => {
+            const timerText = document.querySelector('#timer');
+            const startTime = Date.now();
+            if (this.timerId !== undefined) clearInterval(this.timerId);
+            this.timerId = setInterval(() => {
+                const elapsedtime = ((Date.now() - startTime) / 1000).toFixed(2);
+                if (timerText) timerText.textContent = elapsedtime.toString();
+            }, 10);
+        }
+
+        panelCount: number;
+        numbers: number[];
+        board: Board;
+        currentNum: number | undefined;
+        timerId: NodeJS.Timeout | undefined;
+    }
+
+    new Game(10);
+
 }
